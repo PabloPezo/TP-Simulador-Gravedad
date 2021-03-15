a1b671ffc60be8fee801b7019b9a1c42e3343aa3
Pablo Lozano <paloza01@ucm.es>
2021-03-15 18:37:42 +0100

    Aqui esta creo

--- src/simulator/factories/Builder.java
+++ src/simulator/factories/Builder.java
@@ -1,6 +1,40 @@
 package simulator.factories;
 
-public class Builder 
-{
+import org.json.JSONObject;
+
+public abstract class Builder<T> {
+
+    private  String _typeTag;
+    protected String _desc; // para la parte 2 
+
+
+
+    public T createInstance(JSONObject info)// en cada clase se sabe que new hacer por ejemple en BasicBodyBuilder un new Body
+    {
+        T b=null;
+        if(_typeTag!=null&&_typeTag.equals(info.get("type")))
+        {
+            b=createTheInstance(info.getJSONObject("data"));
+        }
+        return b;
+    }
+    protected abstract T createTheInstance(JSONObject jsonObject);
+    
+
+    public JSONObject getBuilderInfo()
+    {
+        JSONObject info=new JSONObject();
+        info.put("type", _typeTag);
+        info.put("data", createData()); 
+        info.put("desc",_desc);
+        return info;
+    }
+
+
+    protected JSONObject createData()
+    {
+        return new JSONObject();
+    }
+
 
 }
