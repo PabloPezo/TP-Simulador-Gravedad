a3ff6a9dd9781e5ffe2c55222b74b34ba5cc62cb
Pablo Lozano <paloza01@ucm.es>
2021-04-09 13:23:00 +0200

    asdasd

---
+++ Cosas anteriores/src/simulator/control/Controller.java
@@ -0,0 +1,91 @@
+package simulator.control;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import simulator.factories.Factory;
+import simulator.model.Body;
+import simulator.model.PhysicsSimulator;
+
+public class Controller 
+{
+	private PhysicsSimulator _phySimulator;
+	private Factory<Body> _bodiesFactory;
+	
+	public Controller(PhysicsSimulator phySimulator, Factory<Body> bodiesFactory)
+	{
+		_phySimulator = phySimulator;
+		_bodiesFactory = bodiesFactory;
+	}
+	
+	public void loadBodies(InputStream in)
+    {
+        JSONObject js = new JSONObject(new JSONTokener(in));
+        JSONArray bodies = js.getJSONArray("bodies");
+
+        for(int i = 0; i < bodies.length(); i++)
+        {
+        	_phySimulator.addBody(_bodiesFactory.createInstance(bodies.getJSONObject(i)));
+        }
+    }	
+	
+	public void run(int steps, OutputStream out, InputStream expOut, StateComparator cmp) throws NotEqualStatesException
+	{
+		JSONObject expOutJO = null;
+		
+		if(expOut != null)
+		{
+			expOutJO = new JSONObject (new JSONTokener(expOut));
+		}
+		
+		if(out == null)
+		{
+			out = new OutputStream() { public void write(int b)throws IOException {}};
+		}
+		
+		PrintStream p = new PrintStream(out);
+		p.println("{");
+		p.println("\"states\": [");
+		
+		JSONObject expectatedState = null;
+		JSONObject currentState = null;
+		
+		currentState = _phySimulator.getState();
+
+
+        p.println(currentState);
+		p.print(","); 
+        
+		if(expOutJO != null)
+		{
+			expectatedState = expOutJO.getJSONArray("states").getJSONObject(0);
+			if(!cmp.equal(expectatedState, currentState)) throw new NotEqualStatesException(expectatedState, currentState, expectatedState, currentState, 0);
+		}
+
+		for(int i=1; i <= steps; i++)
+		{
+			_phySimulator.advance();
+			currentState = _phySimulator.getState();
+
+			p.println( currentState);
+
+
+			if(i != steps) { p.print(","); }
+
+			if(expOutJO != null)
+			{
+				expectatedState = expOutJO.getJSONArray("states").getJSONObject(i);
+				if(!cmp.equal(expectatedState, currentState)) throw new NotEqualStatesException(expectatedState, currentState, expectatedState, currentState, i);
+			}
+		}
+
+		p.println("]");
+		p.println("}");
+    }	
+}
