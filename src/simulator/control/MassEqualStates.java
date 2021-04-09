a53b0fc8d2216acae3df28fa7fa90dc0fdc89501
Pablo Magno Pezo Ortiz <pablompe@ucm.es>
2021-04-09 13:30:53 +0200

    fgfdg

--- src/simulator/control/MassEqualStates.java
+++ src/simulator/control/MassEqualStates.java
@@ -16,7 +16,6 @@ public class MassEqualStates implements StateComparator
 
 	    for(int i = 0; i < js1.length(); i++)
 	    {
-	    	//js1.getJSONObject(i).getString("id") !=js2.getJSONObject(i).getString("id") 
 	    	if(js1.getJSONObject(i).getString("id").equals(js2.getJSONObject(i).getString("id") )|| js1.getJSONObject(i).getDouble("m")!=js2.getJSONObject(i).getDouble("m")) 
 	    	{
 	    		return false;
