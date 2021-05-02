775ddbbb31134f37a06f3242cb31f124fd1ad331
Pablo Magno Pezo Ortiz <pablompe@ucm.es>
2021-05-02 12:30:44 +0200

    ddd

--- src/simulator/viewer/MainWindow.java
+++ src/simulator/viewer/MainWindow.java
@@ -1,9 +1,12 @@
 package simulator.viewer;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import simulator.control.Controller;
 
@@ -20,12 +23,36 @@ public class MainWindow extends JFrame
 	private void initGUI() 
 	{
 		JPanel mainPanel = new JPanel(new BorderLayout());
-		setContentPane(mainPanel);
-		// TODO complete this method to build the GUI7. Cambios en la clase Main 13
 		this.add(mainPanel);
-		this.setVisible(true);
+		
+		JPanel contentPanel = new JPanel();
+		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+		mainPanel.add(contentPanel, BorderLayout.CENTER);
+		
+		ControlPanel ctrlPanel = new ControlPanel(_ctrl);
+		
+		BodiesTable bodiesInfo = new BodiesTable(_ctrl);
+		
+		Viewer universeViewer = new Viewer(_ctrl);
+		
+		StatusBar statusBar = new StatusBar(_ctrl);
+		
+		mainPanel.add(ctrlPanel, BorderLayout.PAGE_START);
+		mainPanel.add(statusBar, BorderLayout.PAGE_END);
+		
+		bodiesInfo.setPreferredSize(new Dimension(800, 300));
+		contentPanel.add(bodiesInfo);
+		
+		universeViewer.setPreferredSize(new Dimension(800, 600));
+		contentPanel.add(new JScrollPane(universeViewer, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED));
 		
 	}
 	// other private/protected methods
 	// ...
+	
+	public static void main(String[] args)
+	{
+		MainWindow pepe = new MainWindow(new Controller(null, null));
+		//pepe.initGUI();
+	}
 }
