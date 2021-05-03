ec0042ff862d2ed6e065bc441ecf5166cf709677
Pablo Magno Pezo Ortiz <pablompe@ucm.es>
2021-05-03 18:15:12 +0200

    g

--- src/simulator/viewer/ControlPanel.java
+++ src/simulator/viewer/ControlPanel.java
@@ -1,8 +1,18 @@
 package simulator.viewer;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.util.List;
 
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 
 import simulator.control.Controller;
@@ -21,8 +31,27 @@ public class ControlPanel extends JPanel implements SimulatorObserver
 		initGUI();
 		_ctrl.addObserver(this);
 	}
+	
 	private void initGUI()
 	{
+		JFrame mainFrame = new JFrame("PhysicsSimulator");
+		JPanel mainPanel = new JPanel(new BorderLayout());
+
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setLayout(new BorderLayout());
+
+		mainPanel.add (createButton(new ImageIcon("C:/Users/pablo/eclipse-workspace/PhysicsSimulator/resources/icons/open.png"), "ARCHIVOS"));//, BorderLayout.WEST);//, BorderLayout.PAGE_START);
+		mainPanel.add (createButton(new ImageIcon("C:/Users/pablo/eclipse-workspace/PhysicsSimulator/resources/icons/physics.png"), "PHYSICS"));//, BorderLayout.CENTER);
+		mainPanel.add (createButton(new ImageIcon("C:/Users/pablo/eclipse-workspace/PhysicsSimulator/resources/icons/run.png"), "RUN"));//, BorderLayout.WEST);//, BorderLayout.CENTER);
+		mainPanel.add (createButton(new ImageIcon("C:/Users/pablo/eclipse-workspace/PhysicsSimulator/resources/icons/stop.png"), "STOP"));//, BorderLayout.EAST);//, BorderLayout.CENTER);
+		mainPanel.add (createButton(new ImageIcon("C:/Users/pablo/eclipse-workspace/PhysicsSimulator/resources/icons/exit.png"), "EXIT"));//, BorderLayout.EAST);//, BorderLayout.CENTER);
+
+		mainFrame.add(mainPanel);
+		mainFrame.setBounds(400, 300, 800, 90);
+		
+		mainFrame.setVisible(true);
+		mainPanel.setVisible(true);
+		
 	}
 	
 	@SuppressWarnings("unused")
@@ -53,6 +82,12 @@ public class ControlPanel extends JPanel implements SimulatorObserver
 			_stopped = true;
 			// TODO enable all buttons
 		}
+	}
+	
+	private JButton createButton(Icon route, String caption) {
+		JButton button  = new JButton();
+		button.setIcon(route);
+		return button;
 	}
 	
 	// Cositas que me pone solo
