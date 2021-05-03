f051e371ec51510fc74b807c575614338340aad0
Pablo Magno Pezo Ortiz <pablompe@ucm.es>
2021-05-03 20:49:26 +0200

    Volvemos
    Ahora si que si

---
+++ src/simulator/viewer/ControlPanel.java
@@ -0,0 +1,103 @@
+package simulator.viewer;
+
+import java.util.List;
+
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import simulator.control.Controller;
+import simulator.model.Body;
+import simulator.model.SimulatorObserver;
+
+public class ControlPanel extends JPanel implements SimulatorObserver 
+{
+	private static final long serialVersionUID = 1L;
+	private Controller _ctrl;
+	private boolean _stopped;
+	ControlPanel(Controller ctrl) 
+	{
+		_ctrl = ctrl;
+		_stopped = true;
+		initGUI();
+		_ctrl.addObserver(this);
+	}
+	private void initGUI()
+	{
+	
+	}
+	
+	@SuppressWarnings("unused")
+	private void run_sim(int n) 
+	{
+		if ( n>0 && !_stopped ) 
+		{
+			try {
+				//_ctrl.run(1); // Modificar o hace run override
+			} catch (Exception e) 
+			{
+				// TODO show the error in a dialog box
+				// TODO enable all buttons
+				_stopped = true;
+				return;
+			}
+			SwingUtilities.invokeLater( new Runnable() 
+			{
+				@Override
+				public void run() 
+				{
+					run_sim(n-1);
+				}
+			});
+		} 
+		else 
+		{
+			_stopped = true;
+			// TODO enable all buttons
+		}
+	}
+	
+	// Cositas que me pone solo
+	
+	@Override
+	public void onRegister(List<Body> bodies, double time, double dt, String fLawsDesc) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onReset(List<Body> bodies, double time, double dt, String fLawsDesc) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onBodyAdded(List<Body> bodies, Body b) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onAdvance(List<Body> bodies, double time)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onDeltaTimeChanged(double dt) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onForceLawsChanged(String fLawsDesc) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		ControlPanel pepe = new ControlPanel(new Controller(null, null));
+		pepe.initGUI();
+	}
+}
---
+++ src/simulator/viewer/MainWindow.java
@@ -0,0 +1,64 @@
+package simulator.viewer;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import simulator.control.Controller;
+
+public class MainWindow extends JFrame 
+{
+	Controller _ctrl;
+	public MainWindow(Controller ctrl) 
+	{
+		super("Physics Simulator");
+		_ctrl = ctrl;
+		initGUI();
+	}
+	
+	private void initGUI() 
+	{
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		this.add(mainPanel);
+		
+		JPanel contentPanel = new JPanel();
+		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+		mainPanel.add(contentPanel, BorderLayout.CENTER);
+		
+		ControlPanel ctrlPanel = new ControlPanel(_ctrl);
+		
+		BodiesTable bodiesInfo = new BodiesTable(_ctrl);
+		
+		Viewer universeViewer = new Viewer(_ctrl);
+		
+		StatusBar statusBar = new StatusBar(_ctrl);
+		
+		mainPanel.add(ctrlPanel, BorderLayout.PAGE_START);
+		mainPanel.add(statusBar, BorderLayout.PAGE_END);
+		
+		bodiesInfo.setPreferredSize(new Dimension(800, 300));
+		contentPanel.add(bodiesInfo);
+		
+		universeViewer.setPreferredSize(new Dimension(800, 600));
+		contentPanel.add(new JScrollPane(universeViewer, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED));
+		
+	}
+	
+	public void main()
+	{
+		
+	}
+	
+	// other private/protected methods
+	// ...
+	
+	public static void main(String[] args)
+	{
+		MainWindow pepe = new MainWindow(new Controller(null, null));
+		//pepe.initGUI();
+	}
+}
