46e56e2e188c326e08e14eb6e5a4b36eeed85918
Pablo Magno Pezo Ortiz <pablompe@ucm.es>
2021-05-04 18:41:18 +0200

    pusheado

--- src/simulator/viewer/ControlPanel.java
+++ src/simulator/viewer/ControlPanel.java
@@ -3,7 +3,6 @@ package simulator.viewer;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.InputStream;
 import java.util.List;
 
 import javax.swing.Box;
@@ -23,11 +22,18 @@ import simulator.control.Controller;
 import simulator.model.Body;
 import simulator.model.SimulatorObserver;
 
-public class ControlPanel extends JPanel implements SimulatorObserver 
+public class ControlPanel extends JPanel implements SimulatorObserver, ActionListener
 {
 	private static final long serialVersionUID = 1L;
 	private Controller _ctrl;
 	private boolean _stopped;
+
+	private JButton boton1;
+	private JButton boton2;
+	private JButton boton3;
+	private JButton boton4;
+	private JButton boton5;
+
 	ControlPanel(Controller ctrl) 
 	{
 		_ctrl = ctrl;
@@ -35,54 +41,76 @@ public class ControlPanel extends JPanel implements SimulatorObserver
 		initGUI();
 		_ctrl.addObserver(this);
 	}
-	
+
 	private void initGUI()
 	{
 		JFrame mainFrame = new JFrame("PhysicsSimulator");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JPanel mainPanel = new JPanel(new BorderLayout(5, 5));
 
 		JToolBar toolBar = new JToolBar();
 		mainPanel.add(toolBar, BorderLayout.PAGE_START);
 
+		boton1 = createButton(new ImageIcon("resources/icons/open.png"), "Carga el fichero seleccionado");
+		toolBar.add(boton1);
 
-		toolBar.add (createButton(new ImageIcon("resources/icons/open.png"), "Archivos", "Carga el fichero seleccionado"));
 		toolBar.addSeparator();
-		toolBar.add (createButton(new ImageIcon("resources/icons/physics.png"), "Physics", "Cambia las leyes de fuerza"));
+
+		boton2 = createButton(new ImageIcon("resources/icons/physics.png"), "Cambia las leyes de fuerza");
+		toolBar.add(boton2);
+
 		toolBar.addSeparator();
-		toolBar.add (createButton(new ImageIcon("resources/icons/run.png"), "Run", "Inicia la simulación"));
-		toolBar.add (createButton(new ImageIcon("resources/icons/stop.png"), "Stop", "Detiene la simulación"));
+
+		boton3 = createButton(new ImageIcon("resources/icons/run.png"), "Inicia la simulación");
+		toolBar.add(boton3);
+
+		boton4 = createButton(new ImageIcon("resources/icons/stop.png"), "Detiene la simulación");
+		toolBar.add(boton4);
+
+
+
+		//	 	toolBar.add (createButton(new ImageIcon("resources/icons/open.png"), "Archivos", "Carga el fichero seleccionado"));
+		//		toolBar.addSeparator();
+		//		toolBar.add (createButton(new ImageIcon("resources/icons/physics.png"), "Physics", "Cambia las leyes de fuerza"));
+		//		toolBar.addSeparator();
+		//		toolBar.add (createButton(new ImageIcon("resources/icons/run.png"), "Run", "Inicia la simulación"));
+		//		toolBar.add (createButton(new ImageIcon("resources/icons/stop.png"), "Stop", "Detiene la simulación"));
+		//toolBar.add (createButton(new ImageIcon("resources/icons/exit.png"), "Exit", "Cierra la simulación"));
 
 		toolBar.add(new JLabel(" Steps: "));
 		JSpinner steps = new JSpinner();
 		steps.setSize(1, 1);
 		toolBar.add(steps);
-		
+
 		toolBar.add(new JLabel(" Delta-Time: "));
 		JTextField time = new JTextField();
 		toolBar.add(time);
-		
-	
+
+
 		toolBar.add(Box.createHorizontalGlue());
-		toolBar.add (createButton(new ImageIcon("resources/icons/exit.png"), "Exit", "Cierra la simulación"));
-		
+
+		boton5 = createButton(new ImageIcon("resources/icons/exit.png"), "Cierra la simulación");
+		toolBar.add(boton5);
+
 		mainFrame.add(mainPanel);
 		mainFrame.setBounds(400, 300, 800, 90);
-		
-		
+
+
 		mainFrame.setVisible(true);
 		mainPanel.setVisible(true);
 	}
-	
+
 	@SuppressWarnings("unused")
 	private void run_sim(int n) 
 	{
 		if ( n>0 && !_stopped ) 
 		{
-			try {
+			try 
+			{
 				//_ctrl.run(1); // Modificar o hace run override
-			} catch (Exception e) 
+			} 
+			catch (Exception e) 
 			{
 				// TODO show the error in a dialog box
 				// TODO enable all buttons
@@ -104,141 +132,88 @@ public class ControlPanel extends JPanel implements SimulatorObserver
 			// TODO enable all buttons
 		}
 	}
-	
-	public JButton createButton(Icon route, String caption, String toolTip) 
-	{
 
-	    //EventoEleccion eventoBotones = new EventoEleccion(this);
+	public JButton createButton(Icon route, String toolTip) 
+	{
 		JButton button  = new JButton();
 		button.setIcon(route);
 		button.setToolTipText(toolTip);
-		button.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg)
-			{
-				if (caption == "Archivos")
-				{
-					//System.out.println("Archivos");
-					
-					JFileChooser fileChooser = new JFileChooser();
-					int seleccion = fileChooser.showOpenDialog(getParent());
-					
-					_ctrl.reset();	//Limpia el simulador
-					
-					//_ctrl.loadBodies(seleccion);
-					
-				}
-				else if (caption == "Physics")
-				{
-					System.out.println("Physics");
-				}
-				else if (caption == "Run")
-				{
-					System.out.println("Run");
-				}
-				else if (caption == "Stop")
-				{
-					//System.out.println("Stop");
-					
-					_stopped = true;
-				}
-				else
-				{
-					System.exit(0);
-				}
-				
-			}
-		});
+		button.addActionListener(this);
 		return button;
-		
 	}
-	
+
 	// Cositas que me pone solo
-	
+
 	@Override
 	public void onRegister(List<Body> bodies, double time, double dt, String fLawsDesc) 
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 	@Override
 	public void onReset(List<Body> bodies, double time, double dt, String fLawsDesc) 
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 	@Override
 	public void onBodyAdded(List<Body> bodies, Body b) 
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 	@Override
 	public void onAdvance(List<Body> bodies, double time)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 	@Override
 	public void onDeltaTimeChanged(double dt) 
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 	@Override
 	public void onForceLawsChanged(String fLawsDesc) 
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public static void main(String[] args)
 	{
 		ControlPanel pepe = new ControlPanel(new Controller(null, null));
 		pepe.initGUI();
 	}
-}
-
-class EventoEleccion implements ActionListener
-{
-	private ControlPanel eleccion;
-	private JButton boton1 = new JButton("Archivos");
-	private JButton boton2 = new JButton("Physics");
-	private JButton boton3 = new JButton("Run");
-	private JButton boton4 = new JButton("Stop");
-	private JButton boton5 = new JButton("Exit");
-
-	public EventoEleccion(ControlPanel eleccion) 
-	{
-		this.eleccion = eleccion;
-	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) 
-	{   
-		JButton source = (JButton) e.getSource();
-
-		if (boton1 != source  )
+	public void actionPerformed(ActionEvent e)
+	{
+		if (e.getSource() == boton1)
 		{
-			System.out.println("hola");
+			JFileChooser fileChooser = new JFileChooser();
+			int seleccion = fileChooser.showOpenDialog(getParent());
+			_ctrl.reset();	
+			//_ctrl.loadBodies(seleccion);
 		}
-		else if (boton2 == source )
+		else if (e.getSource() == boton2)
 		{
-
+			System.out.println("Physics");
 		}
-		else if (boton3 == source )
+		else if (e.getSource() == boton3)
 		{
-
-		}		
-		else if (boton4 == source)
-		{
-
+			System.out.println("Run");
 		}
-		else// if (boton5 == source)
+		else if (e.getSource() == boton4)
 		{
-			System.out.println("funciona");
+			_stopped = true;
 		}
-	//	https://es.stackoverflow.com/questions/130285/cambiar-tama%C3%B1o-o-estilo-de-botones-java
+		else if(e.getSource() == boton5)
+		{
+			System.exit(0);
+		}
 	}
-
 }
+
+//https://es.stackoverflow.com/questions/130285/cambiar-tama%C3%B1o-o-estilo-de-botones-java
