91738b1c6d8dead161dffecf0c9f5a8956758c70
Pablo Lozano <paloza01@ucm.es>
2021-05-14 12:35:08 +0200

    Merge

--- src/simulator/factories/NewtonUniversalGravitationBuilder.java
+++ src/simulator/factories/NewtonUniversalGravitationBuilder.java
@@ -22,7 +22,7 @@ public class NewtonUniversalGravitationBuilder extends Builder<ForceLaws>
 
 		try
 		{
-			double gConst = js.has("g") ? js.getDouble("g") : 6.67E-11;
+			double gConst = js.has("Ga") ? js.getDouble("Ga") : 6.67E-11;
 			
 //			double gConst = 6.67E-11;
 			
